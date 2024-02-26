c4660e736871e72526e15914818777b010148cf8
piotr gwozdz <piotr.gwozdz@autenti.com>
2024-02-26 15:04:18 +0100

    fix /healtz =)

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.use(express.static('content'));
 app.get('/', (req, res) => {
   res.status(404).sendFile('index.html');
 });
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(__dirname + '/content/index.html');
-});
 
 app.get('/healthz', (req, res) => {
   res.send('Healthy!').end();
+});
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(__dirname + '/content/index.html');
 });
 
 app.listen(8080, () => console.log('App listening on port 8080'));
